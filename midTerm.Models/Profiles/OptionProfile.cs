6b831c2ab0f094762a5cbc46fce7f526483cccb6
andrej popovski <andrejuny@hotmail.com>
2021-05-12 17:58:07 +0200

    Add project files.

---
+++ midTerm.Models/Profiles/OptionProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using midTerm.Data.Entities;
+using midTerm.Models.Models.Option;
+
+namespace midTerm.Models.Profiles
+{
+    class OptionProfile : Profile
+    {
+        public OptionProfile()
+        {
+            CreateMap<Option, OptionBaseModel>()
+                .ReverseMap();
+            CreateMap<Option, OptionModelExtended>()
+                .ReverseMap();
+
+            CreateMap<OptionCreateModel, Option>();
+            CreateMap<OptionUpdateModel, Option>();
+        }
+       
+    }
+}
